d0bcbfeb97672258dd9f4442c8c991fc9a69b7be
Keivan Kazemi <104168492+kaze0017@users.noreply.github.com>
2023-11-27 12:03:53 -0500

    disable  https

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { fileURLToPath } from "url";
 
 import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
+// import mkcert from "vite-plugin-mkcert";
 
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), mkcert()],
+  // plugins: [vue(), mkcert()],
+  plugins: [vue()],
   server: { https: true },
   resolve: {
     alias: {
